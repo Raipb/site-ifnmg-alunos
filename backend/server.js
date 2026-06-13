5bc708fdcdd06adf7e93530025bdc0df66727fc8
Raí Pereira Batista <166126268+Raipb@users.noreply.github.com>
2026-06-12 22:44:17 -0300

    fix: configura CORS para aceitar Vercel

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,12 @@ const bolsasRoutes = require("./Routes/bolsas");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://portal-do-ingressante-ifnmg.vercel.app",
+    "http://localhost:5173"
+  ]
+}));
 app.use(express.json());
 
 app.use("/avisos", avisosRoutes);
@@ -21,7 +26,6 @@ app.use("/editais", editaisRoutes);
 app.use("/auth", authRoutes);
 app.use("/cursos", cursosRoutes);
 app.use("/horarios", horariosRoutes);
-app.use("/auth", authRoutes);
 app.use("/contatos", contatoRoutes);
 app.use("/bolsas", bolsasRoutes);
 
